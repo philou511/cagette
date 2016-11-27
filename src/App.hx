1cd45a0d7cbf20ecd05471faa413d1263b9bc623
Eric Terree <eric.terree@libertysurf.fr>
2016-11-27 19:16:54 +0100

    Change the displayed version from "july" to the date of the last commit

--- src/App.hx
+++ src/App.hx
@@ -15,7 +15,8 @@ class App extends sugoi.BaseApp {
 	 * Version management
 	 * @doc https://github.com/fponticelli/thx.semver
 	 */ 
-	public static var VERSION = ([0,9,2]  : Version).withPre("july");
+	//public static var VERSION = ([0,9,2]  : Version).withPre("july");
+	public static var VERSION = ([0,9,2]  : Version).withPre(MyMacros.getGitCommitDate());
 	
 	public static function main() {
 		
