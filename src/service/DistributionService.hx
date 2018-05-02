9dfa98f98e55456922895c639fc61bcf079b9a44
web-wizard <julie_barbic@yahoo.fr>
2018-05-02 18:30:23 +0200

    Added Create and Edit functions for Distribution Service.

--- src/service/DistributionService.hx
+++ src/service/DistributionService.hx
@@ -1,4 +1,5 @@
 package service;
+import Common;
 
 /**
  * Distribution Service
@@ -26,13 +27,26 @@ class DistributionService
 		var view = App.current.view;
 		var c = d.contract;
 
+		var distribs1;
+		var distribs2;	
+		var distribs3;	
 		//We are checking that there is no existing distribution with an overlapping time frame for the same place and contract
-		//Looking for existing distributions with a time range overlapping the start of the about to be created distribution
-		var distribs1 = db.Distribution.manager.search($contract == c && $place == d.place && $date <= d.date && $end >= d.date, false);
-		//Looking for existing distributions with a time range overlapping the end of the about to be created distribution
-		var distribs2 = db.Distribution.manager.search($contract == c && $place == d.place && $date <= d.end && $end >= d.end, false);	
-		//Looking for existing distributions with a time range included in the time range of the about to be created distribution		
-		var distribs3 = db.Distribution.manager.search($contract == c && $place == d.place && $date >= d.date && $end <= d.end, false);	
+		if (d.id == null) { //We need to check there the id as $id != null doesn't work in the manager.search
+			//Looking for existing distributions with a time range overlapping the start of the about to be created distribution
+			distribs1 = db.Distribution.manager.search($contract == c && $place == d.place && $date <= d.date && $end >= d.date, false);
+			//Looking for existing distributions with a time range overlapping the end of the about to be created distribution
+			distribs2 = db.Distribution.manager.search($contract == c && $place == d.place && $date <= d.end && $end >= d.end, false);	
+			//Looking for existing distributions with a time range included in the time range of the about to be created distribution		
+			distribs3 = db.Distribution.manager.search($contract == c && $place == d.place && $date >= d.date && $end <= d.end, false);	
+		}
+		else {
+			//Looking for existing distributions with a time range overlapping the start of the about to be created distribution
+			distribs1 = db.Distribution.manager.search($contract == c && $place == d.place && $date <= d.date && $end >= d.date && $id != d.id, false);
+			//Looking for existing distributions with a time range overlapping the end of the about to be created distribution
+			distribs2 = db.Distribution.manager.search($contract == c && $place == d.place && $date <= d.end && $end >= d.end && $id != d.id, false);	
+			//Looking for existing distributions with a time range included in the time range of the about to be created distribution		
+			distribs3 = db.Distribution.manager.search($contract == c && $place == d.place && $date >= d.date && $end <= d.end && $id != d.id, false);	
+		}
 			
 		if (distribs1.length != 0 || distribs2.length != 0 || distribs3.length != 0) {
 			throw t._("There is already a distribution at this place overlapping with the time range you've selected.");
@@ -44,6 +58,113 @@ class DistributionService
 		if (c.type == db.Contract.TYPE_VARORDER ) {
 			if (d.date.getTime() < d.orderEndDate.getTime() ) throw t._("The distribution start date must be set after the orders end date.");
 			if (d.orderStartDate.getTime() > d.orderEndDate.getTime() ) throw t._("The orders end date must be set after the orders start date !");
+		}
+
+	}
+
+	/**
+	* Creates a new distribution and prevents distribution overlapping and other checks
+	*  @param contract
+	*  @param text
+	*  @param date
+	*  @param end
+	*  @param placeId
+	*  @param distributor1Id
+	*  @param distributor2Id
+	*  @param distributor3Id
+	*  @param distributor4Id
+	*  @param orderStartDate
+	*  @param orderEndDate
+	*/
+	 public static function create(contract:db.Contract,text:String,date:Date,end:Date,placeId:Int,
+	 	distributor1Id:Int,distributor2Id:Int,distributor3Id:Int,distributor4Id:Int,
+		orderStartDate:Date,orderEndDate:Date):db.Distribution {
+		
+		var d = new db.Distribution();
+		d.contract = contract;
+		d.text = text;
+		d.date = date;
+		d.place = db.Place.manager.get(placeId);
+		d.distributor1 = db.User.manager.get(distributor1Id);
+		d.distributor2 = db.User.manager.get(distributor2Id);
+		d.distributor3 = db.User.manager.get(distributor3Id);
+		d.distributor4 = db.User.manager.get(distributor4Id);
+		if(contract.type==db.Contract.TYPE_VARORDER){
+			d.orderStartDate = orderStartDate;
+			d.orderEndDate = orderEndDate;
+		}
+					
+		if (end == null) {
+			d.end = DateTools.delta(d.date, 1000.0 * 60 * 60);
+		}
+		else {
+			d.end = new Date(d.date.getFullYear(), d.date.getMonth(), d.date.getDate(), end.getHours(), end.getMinutes(), 0);
+		} 
+		
+		DistributionService.checkDistrib(d);
+		
+		var e :Event = NewDistrib(d);
+		App.current.event(e);
+		
+		if (d.date == null){
+			return null;
+		} 
+		else {
+			d.insert();
+			return d;
+		}
+	}
+
+	/**
+	* Modifies an existing distribution and prevents distribution overlapping and other checks
+	*  @param d
+	*  @param text
+	*  @param date
+	*  @param end
+	*  @param placeId
+	*  @param distributor1Id
+	*  @param distributor2Id
+	*  @param distributor3Id
+	*  @param distributor4Id
+	*  @param orderStartDate
+	*  @param orderEndDate
+	*/
+	 public static function edit(d:db.Distribution,text:String,date:Date,end:Date,placeId:Int,
+	 	distributor1Id:Int,distributor2Id:Int,distributor3Id:Int,distributor4Id:Int,
+		orderStartDate:Date,orderEndDate:Date):db.Distribution {
+
+		//We prevent others from modifying it
+		d.lock();
+
+		d.text = text;
+		d.date = date;
+		d.place = db.Place.manager.get(placeId);
+		d.distributor1 = db.User.manager.get(distributor1Id);
+		d.distributor2 = db.User.manager.get(distributor2Id);
+		d.distributor3 = db.User.manager.get(distributor3Id);
+		d.distributor4 = db.User.manager.get(distributor4Id);
+		if(d.contract.type==db.Contract.TYPE_VARORDER){
+			d.orderStartDate = orderStartDate;
+			d.orderEndDate = orderEndDate;
+		}
+					
+		if (end == null) {
+			d.end = DateTools.delta(d.date, 1000.0 * 60 * 60);
+		}
+		else {
+			d.end = new Date(d.date.getFullYear(), d.date.getMonth(), d.date.getDate(), end.getHours(), end.getMinutes(), 0);
+		} 
+		
+		DistributionService.checkDistrib(d);
+
+		App.current.event(EditDistrib(d));
+		
+		if (d.date == null){
+			return null;
+		} 
+		else {
+			d.update();
+			return d;
 		}
 	}
 }
--- src/test/TestDistributions.hx
+++ src/test/TestDistributions.hx
@@ -22,52 +22,34 @@ class TestDistributions extends haxe.unit.TestCase
 	 * Check that we can add a distribution outside existing time range for a specific contract and place
 	 * Check that we can't add a distribution overlapping with existing distribution for a specific contract and place
 	 */
-	function testUnicity(){        
+	function testOverlapping() {        
     	var existingDistrib = TestSuite.DISTRIB_FRUITS_PLACE_DU_VILLAGE;
-		var distrib1 = new db.Distribution();
-		distrib1.date = new Date(2018, 5, 1, 18, 0, 0);
-		distrib1.end = new Date(2018, 5, 1, 18, 30, 0);
-		distrib1.orderStartDate = new Date(2018, 4, 1, 18, 0, 0);
-		distrib1.orderEndDate = new Date(2018, 4, 30, 18, 30, 0);
-		distrib1.contract = existingDistrib.contract;
-		distrib1.place = existingDistrib.place;
 		var e1 = null;
-		try{
-			service.DistributionService.checkDistrib(distrib1);
+		try {
+			var distrib1 = service.DistributionService.create(existingDistrib.contract,null,new Date(2018, 5, 1, 18, 0, 0),new Date(2018, 5, 1, 18, 30, 0),
+			existingDistrib.place.id,null,null,null,null,new Date(2018, 4, 1, 18, 0, 0),new Date(2018, 4, 30, 18, 30, 0));
 		}
-		catch(x:String){
+		catch(x:String) {
 			e1 = x;
 		}
 		assertEquals(e1, null);
 
 		//existingDistrib.date <= distrib2.date && existingDistrib.end >= distrib2.date
-		var distrib2 = new db.Distribution();
-		distrib2.date = new Date(2017, 5, 1, 19, 30, 0);
-		distrib2.end = new Date(2017, 5, 1, 20, 30, 0);
-		distrib2.orderStartDate = new Date(2017, 4, 1, 18, 0, 0);
-		distrib2.orderEndDate = new Date(2017, 4, 30, 18, 30, 0);
-		distrib2.contract = existingDistrib.contract;
-		distrib2.place = existingDistrib.place;
 		var e2 = null;
-		try{
-			service.DistributionService.checkDistrib(distrib2);
+		try {
+			var distrib2 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 5, 1, 19, 30, 0),new Date(2017, 5, 1, 20, 30, 0),
+			existingDistrib.place.id,null,null,null,null,new Date(2017, 4, 1, 18, 0, 0),new Date(2017, 4, 30, 18, 30, 0));
 		}
-		catch(x:String){
+		catch(x:String) {
 			e2 = x;
 		}
 		assertEquals(e2, "There is already a distribution at this place overlapping with the time range you've selected.");
 
 		//existingDistrib.date <= distrib3.end && existingDistrib.end >= distrib3.end
-		var distrib3 = new db.Distribution();
-		distrib3.date = new Date(2017, 5, 1, 17, 30, 0);
-		distrib3.end = new Date(2017, 5, 1, 19, 30, 0);
-		distrib3.orderStartDate = new Date(2017, 4, 1, 18, 0, 0);
-		distrib3.orderEndDate = new Date(2017, 4, 30, 18, 30, 0);
-		distrib3.contract = existingDistrib.contract;
-		distrib3.place = existingDistrib.place;
 		var e3 = null;
 		try{
-			service.DistributionService.checkDistrib(distrib3);
+			var distrib3 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 19, 30, 0),
+			existingDistrib.place.id,null,null,null,null,new Date(2017, 4, 1, 18, 0, 0),new Date(2017, 4, 30, 18, 30, 0));
 		}
 		catch(x:String){
 			e3 = x;
@@ -75,16 +57,10 @@ class TestDistributions extends haxe.unit.TestCase
 		assertEquals(e3, "There is already a distribution at this place overlapping with the time range you've selected.");
 
 		//existingDistrib.date >= distrib4.date && existingDistrib.end <= distrib4.end
-		var distrib4 = new db.Distribution();
-		distrib4.date = new Date(2017, 5, 1, 17, 30, 0);
-		distrib4.end = new Date(2017, 5, 1, 21, 30, 0);
-		distrib4.orderStartDate = new Date(2017, 4, 1, 18, 0, 0);
-		distrib4.orderEndDate = new Date(2017, 4, 30, 18, 30, 0);
-		distrib4.contract = existingDistrib.contract;
-		distrib4.place = existingDistrib.place;
 		var e4 = null;
 		try{
-			service.DistributionService.checkDistrib(distrib4);
+			var distrib4 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 21, 30, 0),
+			existingDistrib.place.id,null,null,null,null,new Date(2017, 4, 1, 18, 0, 0),new Date(2017, 4, 30, 18, 30, 0));
 		}
 		catch(x:String){
 			e4 = x;
@@ -92,16 +68,10 @@ class TestDistributions extends haxe.unit.TestCase
 		assertEquals(e4, "There is already a distribution at this place overlapping with the time range you've selected.");
 
 		//existingDistrib.date  > distrib5.end
-		var distrib5 = new db.Distribution();
-		distrib5.date = new Date(2017, 5, 1, 17, 30, 0);
-		distrib5.end = new Date(2017, 5, 1, 18, 59, 0);
-		distrib5.orderStartDate = new Date(2017, 4, 1, 18, 0, 0);
-		distrib5.orderEndDate = new Date(2017, 4, 30, 18, 30, 0);
-		distrib5.contract = existingDistrib.contract;
-		distrib5.place = existingDistrib.place;
 		var e5 = null;
 		try{
-			service.DistributionService.checkDistrib(distrib5);
+			var distrib5 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 18, 59, 0),
+			existingDistrib.place.id,null,null,null,null,new Date(2017, 4, 1, 18, 0, 0),new Date(2017, 4, 30, 18, 30, 0));
 		}
 		catch(x:String){
 			e5 = x;
@@ -109,16 +79,10 @@ class TestDistributions extends haxe.unit.TestCase
 		assertEquals(e5, null);
 
 		//existingDistrib.date  > distrib6.end
-		var distrib6 = new db.Distribution();
-		distrib6.date = new Date(2017, 5, 1, 17, 30, 0);
-		distrib6.end = new Date(2017, 5, 1, 18, 59, 0);
-		distrib6.orderStartDate = new Date(2017, 4, 1, 18, 0, 0);
-		distrib6.orderEndDate = new Date(2017, 5, 30, 18, 30, 0);
-		distrib6.contract = existingDistrib.contract;
-		distrib6.place = existingDistrib.place;
 		var e6 = null;
 		try{
-			service.DistributionService.checkDistrib(distrib6);
+			var distrib6 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 3, 1, 17, 30, 0),new Date(2017, 3, 1, 18, 59, 0),
+			existingDistrib.place.id,null,null,null,null,new Date(2017, 2, 1, 18, 0, 0),new Date(2017, 3, 30, 18, 30, 0));
 		}
 		catch(x:String){
 			e6 = x;
@@ -127,4 +91,12 @@ class TestDistributions extends haxe.unit.TestCase
 
 	}
 
+	/**
+	 * Check that we can add a distribution outside existing time range for a specific contract and place
+	 * Check that we can't add a distribution overlapping with existing distribution for a specific contract and place
+	 */
+	function testEdit() { 
+
+		//TO DO
+    
 }
