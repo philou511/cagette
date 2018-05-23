00184db3e59031a1eae7e4ab845a84b6c3beef97
web-wizard <julie_barbic@yahoo.fr>
2018-05-23 19:34:21 +0200

    Refactored createCycleDistribs to use the create function from DistributionService and updated the unit tests.

--- src/db/DistributionCycle.hx
+++ src/db/DistributionCycle.hx
@@ -1,7 +1,6 @@
 package db;
 import sys.db.Object;
 import sys.db.Types;
-import datetime.DateTime;
 using tools.DateTool;
 
 enum CycleType {
@@ -42,110 +41,91 @@ class DistributionCycle extends Object
 			"daysBeforeOrderEnd"		=> "Fermeture de commande (nbre de jours avant distribution)",			
 		];
 	}
+
+	public static function getDates(dc:DistributionCycle, datePointer:Date) {
+		var startDate = new Date(datePointer.getFullYear(),datePointer.getMonth(),datePointer.getDate(),dc.startHour.getHours(),dc.startHour.getMinutes(),0);
+		var orderStartDate = null;
+		var orderEndDate = null;
+		if (dc.contract.type == Contract.TYPE_VARORDER){
+			
+			if (dc.daysBeforeOrderEnd == null || dc.daysBeforeOrderStart == null) throw "daysBeforeOrderEnd or daysBeforeOrderStart is null";
+			
+			var a = DateTools.delta(startDate, -1.0 * dc.daysBeforeOrderStart * 1000 * 60 * 60 * 24);
+			var h : Date = dc.openingHour;
+			orderStartDate = new Date(a.getFullYear(), a.getMonth(), a.getDate(), h.getHours(), h.getMinutes(), 0);
+			
+			var a = DateTools.delta(startDate, -1.0 * dc.daysBeforeOrderEnd * 1000 * 60 * 60 * 24);
+			var h : Date = dc.closingHour;
+			orderEndDate = new Date(a.getFullYear(), a.getMonth(), a.getDate(), h.getHours(), h.getMinutes(), 0);			
+		}
+		return { date: startDate, orderStartDate: orderStartDate, orderEndDate: orderEndDate };
+	}
 	
 	/**
 	 * on créé toutes les distribs en partant du jour de la semaine de la premiere date
 	 * 
 	 * @TODO refactor this with http://thx-lib.org/api/thx/Dates.html#jump
 	 */
-	public static function updateAllDistribs(dc:DistributionCycle) {
+	public static function createCycleDistribs(dc:DistributionCycle) {
 		//switch end date to 23:59 to avoid the last distribution to be skipped
 		dc.endDate = tools.DateTool.setHourMinute(dc.endDate,23,59);
-
-		var datePointer = new Date(dc.startDate.getFullYear(), dc.startDate.getMonth(), dc.startDate.getDate(), 12, 0, 0);
-		//why hour=12 ? because if we set hour to 0, it switch to 23 (-1) or 1 (+1) on daylight saving time switch dates, thus changing the day!!
 		
 		if (dc.id == null) throw "this distributionCycle has not been recorded";
 		
-		//first distrib
-		var d = new Distribution();
-		d.contract = dc.contract;
-		d.distributionCycle = dc;
-		d.place = dc.place;
-		d.date = new Date(datePointer.getFullYear(), datePointer.getMonth(), datePointer.getDate(), dc.startHour.getHours(), dc.startHour.getMinutes(), 0);
-		d.end  = new Date(datePointer.getFullYear(), datePointer.getMonth(), datePointer.getDate(), dc.endHour.getHours()  , dc.endHour.getMinutes()  , 0);
-		if (dc.contract.type == Contract.TYPE_VARORDER){
-			
-			if (dc.daysBeforeOrderEnd == null || dc.daysBeforeOrderStart == null) throw "daysBeforeOrderEnd or daysBeforeOrderStart is null";
-			
-			var a = DateTools.delta(d.date, -1.0 * dc.daysBeforeOrderStart * 1000 * 60 * 60 * 24);
-			var h : Date = dc.openingHour;
-			d.orderStartDate = new Date(a.getFullYear(), a.getMonth(), a.getDate(), h.getHours(), h.getMinutes(), 0);
-			
-			var a = DateTools.delta(d.date, -1.0 * dc.daysBeforeOrderEnd * 1000 * 60 * 60 * 24);
-			var h : Date = dc.closingHour;
-			d.orderEndDate = new Date(a.getFullYear(), a.getMonth(), a.getDate(), h.getHours(), h.getMinutes(), 0);			
-		}
-		
-		d.insert();
-
-		var firstDistribDate = d.date;
-		
 		//iterations
+		//For first distrib
+		var datePointer = new Date(dc.startDate.getFullYear(), dc.startDate.getMonth(), dc.startDate.getDate(), 12, 0, 0);
+		//why hour=12 ? because if we set hour to 0, it switch to 23 (-1) or 1 (+1) on daylight saving time switch dates, thus changing the day!!
+		var firstDistribDate = new Date(datePointer.getFullYear(),datePointer.getMonth(),datePointer.getDate(),dc.startHour.getHours(),dc.startHour.getMinutes(),0);
 		for(i in 0...100) {
-			
-			var d = new Distribution();
-			d.contract = dc.contract;
-			d.distributionCycle = dc;
-			d.place = dc.place;
-			
-			//date de la distrib
-			var oneDay = 1000 * 60 * 60 * 24.0;
-			switch(dc.cycleType) {
-				case Weekly :
-					datePointer = DateTools.delta(datePointer, oneDay * 7.0);
-					App.log("on ajoute "+(oneDay * 7.0)+"millisec pour ajouter 7 jours");
-					App.log('pointer : $datePointer');
-					
-				case BiWeekly : 	
-					datePointer = DateTools.delta(datePointer, oneDay * 14.0);
-					
-				case TriWeekly : 	
-					datePointer = DateTools.delta(datePointer, oneDay * 21.0);
-					
-				case Monthly :
-				    var n = tools.DateTool.getWhichNthDayOfMonth(firstDistribDate);
-					var dayOfWeek = firstDistribDate.getDay();
-					var nextMonth = new Date(datePointer.getFullYear(), datePointer.getMonth() + 1, 1, 0, 0, 0);
-					datePointer = tools.DateTool.getNthDayOfMonth(nextMonth.getFullYear(), nextMonth.getMonth(), dayOfWeek, n);
-					if (datePointer.getMonth() != nextMonth.getMonth()) {
-						datePointer = tools.DateTool.getNthDayOfMonth(nextMonth.getFullYear(), nextMonth.getMonth(), dayOfWeek, n - 1);
-					}
+
+			if(i != 0){ //All distribs except the first one
+				var oneDay = 1000 * 60 * 60 * 24.0;
+				switch(dc.cycleType) {
+					case Weekly :
+						datePointer = DateTools.delta(datePointer, oneDay * 7.0);
+						App.log("on ajoute "+(oneDay * 7.0)+"millisec pour ajouter 7 jours");
+						App.log('pointer : $datePointer');
+						
+					case BiWeekly : 	
+						datePointer = DateTools.delta(datePointer, oneDay * 14.0);
+						
+					case TriWeekly : 	
+						datePointer = DateTools.delta(datePointer, oneDay * 21.0);
+						
+					case Monthly :
+						var n = tools.DateTool.getWhichNthDayOfMonth(firstDistribDate);
+						var dayOfWeek = firstDistribDate.getDay();
+						var nextMonth = new Date(datePointer.getFullYear(), datePointer.getMonth() + 1, 1, 0, 0, 0);
+						datePointer = tools.DateTool.getNthDayOfMonth(nextMonth.getFullYear(), nextMonth.getMonth(), dayOfWeek, n);
+						if (datePointer.getMonth() != nextMonth.getMonth()) {
+							datePointer = tools.DateTool.getNthDayOfMonth(nextMonth.getFullYear(), nextMonth.getMonth(), dayOfWeek, n - 1);
+						}
+				}
 			}
-			
+					
 			//stop if cycle end is reached
 			if (datePointer.getTime() > dc.endDate.getTime()) {				
 				break;
 			}
 			
-			//set distribution date + end
-			d.date = new Date(datePointer.getFullYear(), datePointer.getMonth(), datePointer.getDate(), dc.startHour.getHours(), dc.startHour.getMinutes(), 0);
-			d.end  = new Date(datePointer.getFullYear(), datePointer.getMonth(), datePointer.getDate(), dc.endHour.getHours(),   dc.endHour.getMinutes(),   0);
+			var dates = getDates(dc, datePointer);
 			
-			//set order opening and closing hours
-			if (dc.contract.type == Contract.TYPE_VARORDER){
-				
-				var a = DateTools.delta(d.date, -1.0 * dc.daysBeforeOrderStart * 1000 * 60 * 60 * 24);
-				var h : Date = dc.openingHour;
-				d.orderStartDate = new Date(a.getFullYear(), a.getMonth(), a.getDate(), h.getHours(), h.getMinutes(), 0);
-				
-				var a = DateTools.delta(d.date, -1.0 * dc.daysBeforeOrderEnd * 1000 * 60 * 60 * 24);
-				var h : Date = dc.closingHour;
-				d.orderEndDate = new Date(a.getFullYear(), a.getMonth(), a.getDate(), h.getHours(), h.getMinutes(), 0);
-			}
-			
-			d.insert();
+			service.DistributionService.create(dc.contract,null,dates.date,
+			new Date(datePointer.getFullYear(),datePointer.getMonth(),datePointer.getDate(),dc.endHour.getHours(),dc.endHour.getMinutes(),0),
+			dc.place.id,null,null,null,null,dates.orderStartDate,dates.orderEndDate,dc);
+
 		}
 	}
 	
 	/**
 	 *  Delete distributions which are part of this cycle
 	 */
-	public function deleteChilds(){
+	public function deleteCycleDistribs(){
 		
-		var childs = db.Distribution.manager.search($distributionCycle == this, true);
+		var children = db.Distribution.manager.search($distributionCycle == this, true);
 		var messages = [];
-		for ( d in childs ){
+		for ( d in children ){
 			
 			if (d.contract.type == db.Contract.TYPE_VARORDER && !d.canDelete() ){
 				var t = sugoi.i18n.Locale.texts;
--- src/test/TestDistributions.hx
+++ src/test/TestDistributions.hx
@@ -122,6 +122,7 @@ class TestDistributions extends haxe.unit.TestCase
 		assertEquals(weeklyDistribs[3].end.toString(), new Date(2019, 0, 14, 14, 0, 0).toString());
 		assertEquals(weeklyDistribs[4].date.toString(), new Date(2019, 0, 21, 13, 0, 0).toString());
 		assertEquals(weeklyDistribs[4].end.toString(), new Date(2019, 0, 21, 14, 0, 0).toString());
+		weeklyDistribCycle.deleteCycleDistribs();
 
 		var monthlyDistribCycle = service.DistributionService.createCycle(TestSuite.CONTRAT_LEGUMES,Monthly,new Date(2018, 9, 30, 0, 0, 0),
 		new Date(2019, 2, 31, 0, 0, 0),new Date(2018, 5, 4, 13, 0, 0),new Date(2018, 5, 4, 14, 0, 0),10,2,
@@ -141,6 +142,7 @@ class TestDistributions extends haxe.unit.TestCase
 		assertEquals(monthlyDistribs[4].end.toString(), new Date(2019, 1, 26, 14, 0, 0).toString());
 		assertEquals(monthlyDistribs[5].date.toString(), new Date(2019, 2, 26, 13, 0, 0).toString());
 		assertEquals(monthlyDistribs[5].end.toString(), new Date(2019, 2, 26, 14, 0, 0).toString());
+		monthlyDistribCycle.deleteCycleDistribs();
 
 		var biweeklyDistribCycle = service.DistributionService.createCycle(TestSuite.CONTRAT_LEGUMES,BiWeekly,new Date(2018, 9, 30, 0, 0, 0),
 		new Date(2019, 0, 31, 0, 0, 0),new Date(2018, 5, 4, 13, 0, 0),new Date(2018, 5, 4, 14, 0, 0),10,2,
@@ -162,6 +164,7 @@ class TestDistributions extends haxe.unit.TestCase
 		assertEquals(biweeklyDistribs[5].end.toString(), new Date(2019, 0, 8, 14, 0, 0).toString());
 		assertEquals(biweeklyDistribs[6].date.toString(), new Date(2019, 0, 22, 13, 0, 0).toString());
 		assertEquals(biweeklyDistribs[6].end.toString(), new Date(2019, 0, 22, 14, 0, 0).toString());
+		biweeklyDistribCycle.deleteCycleDistribs();		
 		
 		var triweeklyDistribCycle = service.DistributionService.createCycle(TestSuite.CONTRAT_LEGUMES,TriWeekly,new Date(2018, 9, 30, 0, 0, 0),
 		new Date(2019, 0, 31, 0, 0, 0),new Date(2018, 5, 4, 13, 0, 0),new Date(2018, 5, 4, 14, 0, 0),10,2,
@@ -179,6 +182,7 @@ class TestDistributions extends haxe.unit.TestCase
 		assertEquals(triweeklyDistribs[3].end.toString(), new Date(2019, 0, 1, 14, 0, 0).toString());
 		assertEquals(triweeklyDistribs[4].date.toString(), new Date(2019, 0, 22, 13, 0, 0).toString());
 		assertEquals(triweeklyDistribs[4].end.toString(), new Date(2019, 0, 22, 14, 0, 0).toString());
+		triweeklyDistribCycle.deleteCycleDistribs();				
 
 		// assertEquals(getNthDayOfMonth(2018,4,3,4).toString(),new Date(2018,4,23,0,0,0).toString());
 		// assertEquals(getNthDayOfMonth(2018,4,2,2).toString(),new Date(2018,4,8,0,0,0).toString());
