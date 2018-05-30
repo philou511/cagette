aa8c0be0f5535f7c4eb923a9638daa2d33681713
web-wizard <julie_barbic@yahoo.fr>
2018-05-30 17:36:00 +0200

    Updated unit tests to catch tink.core.Error exceptions.

--- src/test/TestDistributions.hx
+++ src/test/TestDistributions.hx
@@ -26,10 +26,10 @@ class TestDistributions extends haxe.unit.TestCase
     	var existingDistrib = TestSuite.DISTRIB_FRUITS_PLACE_DU_VILLAGE;
 		var e1 = null;
 		try {
-			var distrib1 = service.DistributionService.create(existingDistrib.contract,null,new Date(2018, 5, 1, 18, 0, 0),new Date(2018, 5, 1, 18, 30, 0),
+			var distrib1 = service.DistributionService.create(existingDistrib.contract,new Date(2018, 5, 1, 18, 0, 0),new Date(2018, 5, 1, 18, 30, 0),
 			existingDistrib.place.id,null,null,null,null,new Date(2018, 4, 1, 18, 0, 0),new Date(2018, 4, 30, 18, 30, 0));
 		}
-		catch(x:String) {
+		catch(x:tink.core.Error) {
 			e1 = x;
 		}
 		assertEquals(e1, null);
@@ -37,43 +37,43 @@ class TestDistributions extends haxe.unit.TestCase
 		//existingDistrib.date <= distrib2.date && existingDistrib.end >= distrib2.date
 		var e2 = null;
 		try {
-			var distrib2 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 5, 1, 19, 30, 0),new Date(2017, 5, 1, 20, 30, 0),
+			var distrib2 = service.DistributionService.create(existingDistrib.contract,new Date(2017, 5, 1, 19, 30, 0),new Date(2017, 5, 1, 20, 30, 0),
 			existingDistrib.place.id,null,null,null,null,new Date(2017, 4, 1, 18, 0, 0),new Date(2017, 4, 30, 18, 30, 0));
 		}
-		catch(x:String) {
+		catch(x:tink.core.Error) {
 			e2 = x;
 		}
-		assertEquals(e2, "There is already a distribution at this place overlapping with the time range you've selected.");
+		assertEquals(e2.message, "There is already a distribution at this place overlapping with the time range you've selected.");
 
 		//existingDistrib.date <= distrib3.end && existingDistrib.end >= distrib3.end
 		var e3 = null;
 		try{
-			var distrib3 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 19, 30, 0),
+			var distrib3 = service.DistributionService.create(existingDistrib.contract,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 19, 30, 0),
 			existingDistrib.place.id,null,null,null,null,new Date(2017, 4, 1, 18, 0, 0),new Date(2017, 4, 30, 18, 30, 0));
 		}
-		catch(x:String){
+		catch(x:tink.core.Error){
 			e3 = x;
 		}
-		assertEquals(e3, "There is already a distribution at this place overlapping with the time range you've selected.");
+		assertEquals(e3.message, "There is already a distribution at this place overlapping with the time range you've selected.");
 
 		//existingDistrib.date >= distrib4.date && existingDistrib.end <= distrib4.end
 		var e4 = null;
 		try{
-			var distrib4 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 21, 30, 0),
+			var distrib4 = service.DistributionService.create(existingDistrib.contract,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 21, 30, 0),
 			existingDistrib.place.id,null,null,null,null,new Date(2017, 4, 1, 18, 0, 0),new Date(2017, 4, 30, 18, 30, 0));
 		}
-		catch(x:String){
+		catch(x:tink.core.Error){
 			e4 = x;
 		}
-		assertEquals(e4, "There is already a distribution at this place overlapping with the time range you've selected.");
+		assertEquals(e4.message, "There is already a distribution at this place overlapping with the time range you've selected.");
 
 		//existingDistrib.date  > distrib5.end
 		var e5 = null;
 		try{
-			var distrib5 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 18, 59, 0),
+			var distrib5 = service.DistributionService.create(existingDistrib.contract,new Date(2017, 5, 1, 17, 30, 0),new Date(2017, 5, 1, 18, 59, 0),
 			existingDistrib.place.id,null,null,null,null,new Date(2017, 4, 1, 18, 0, 0),new Date(2017, 4, 30, 18, 30, 0));
 		}
-		catch(x:String){
+		catch(x:tink.core.Error){
 			e5 = x;
 		}
 		assertEquals(e5, null);
@@ -81,13 +81,13 @@ class TestDistributions extends haxe.unit.TestCase
 		//existingDistrib.date  > distrib6.end
 		var e6 = null;
 		try{
-			var distrib6 = service.DistributionService.create(existingDistrib.contract,null,new Date(2017, 3, 1, 17, 30, 0),new Date(2017, 3, 1, 18, 59, 0),
+			var distrib6 = service.DistributionService.create(existingDistrib.contract,new Date(2017, 3, 1, 17, 30, 0),new Date(2017, 3, 1, 18, 59, 0),
 			existingDistrib.place.id,null,null,null,null,new Date(2017, 2, 1, 18, 0, 0),new Date(2017, 3, 30, 18, 30, 0));
 		}
-		catch(x:String){
+		catch(x:tink.core.Error){
 			e6 = x;
 		}
-		assertEquals(e6, "The distribution start date must be set after the orders end date.");
+		assertEquals(e6.message, "The distribution start date must be set after the orders end date.");
 
 	}
 
