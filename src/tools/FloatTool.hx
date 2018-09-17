6a57e9292d713e0deaf8cecede31bf2b5c9f1455
fbarbut <francois@alilo.fr>
2018-09-17 10:08:57 +0200

    fix productInput react

---
+++ src/tools/FloatTool.hx
@@ -0,0 +1,15 @@
+package tools;
+
+
+class FloatTool{
+
+	/**
+	Tries to fix buddy float comparison in Neko...
+	**/
+	public static function isEqual(a:Float,b:Float){
+		a = Math.round(a*10000);
+		b = Math.round(b*10000);
+		return a==b;
+	}
+
+}
