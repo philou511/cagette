ec7db07a583ed50db158480617aa60e787f55d32
web-wizard <julie_barbic@yahoo.fr>
2020-01-09 19:11:56 +0100

    WIP Moved modification of a subscription to a separate page instead of orderBox. Refacto

--- src/controller/Subscriptions.hx
+++ src/controller/Subscriptions.hx
@@ -38,42 +38,6 @@ class Subscriptions extends controller.Controller
 		checkToken();
 	}
 	
-	@tpl("form.mtt")
-	public function doPrevEdit( subscription : db.Subscription ) {
-
-		if ( !app.user.canManageContract( subscription.catalog ) ) throw Error( '/', t._('Access forbidden') );
-
-		var form = new sugoi.form.Form( 'editsubscription' );
-		var startDatePicker = new sugoi.form.elements.DatePicker( 'startDate', 'Date de début', subscription.startDate );
-		startDatePicker.format = 'LL';
-		form.addElement( startDatePicker );
-		var endDatePicker = new sugoi.form.elements.DatePicker( 'endDate', 'Date de fin', subscription.endDate );
-		endDatePicker.format = 'LL';
-		form.addElement( endDatePicker );
-
-		if ( form.isValid() ) {
-
-			try {
-
-				service.SubscriptionService.updateSubscription( subscription, form.getValueOf( 'startDate' ), form.getValueOf( 'endDate' ) );
-
-			}
-			catch( error : Error ) {
-				
-				throw Error( '/contractAdmin/subscriptions/edit/' + subscription.id, error.message );
-			}
-
-			throw Ok( '/contractAdmin/subscriptions/' + subscription.catalog.id, 'La souscription pour ' + subscription.user.getName() + ' a bien été mise à jour.' );
-		}
-	
-		view.form = form;
-		view.title = 'Modification de dates de la souscription pour ' + subscription.user.getName();
-		view.c = subscription.catalog;
-		view.catalog = subscription.catalog;
-		view.nav.push( 'subscriptions' );
-	}
-
-
 	public function doDelete( subscription : db.Subscription ) {
 		
 		if ( !app.user.canManageContract( subscription.catalog ) ) throw Error( '/', t._('Access forbidden') );
@@ -96,64 +60,6 @@ class Subscriptions extends controller.Controller
 		}
 
 		throw Error( '/contractAdmin/subscriptions/' + subscription.catalog.id, t._("Token error") );
-	}
-
-
-	@tpl("form.mtt")
-	public function doPrevInsert( catalog : db.Catalog ) {
-
-		if ( !app.user.canManageContract( catalog ) ) throw Error( '/', t._('Access forbidden') );
-
-		var subscription = new db.Subscription();
-		var form = new sugoi.form.Form( 'newsubscription' );
-		form.addElement( new sugoi.form.elements.IntSelect( 'userId', 'Membre', app.user.getGroup().getMembersFormElementData(), null, true ) );
-		form.addElement( new sugoi.form.elements.Html( "html", "<hr/>" ) );
-		form.addElement( new sugoi.form.elements.Html( "qty", "Quantité" ) );
-		var catalogProducts = catalog.getProducts();
-		for ( product in catalogProducts ) {
-
-			form.addElement( new sugoi.form.elements.IntInput( "product" + product.id, product.name + " " + product.price + " €", 0, true ) );
-		}
-		form.addElement( new sugoi.form.elements.Html( "html", "<hr/>" ) );
-		var startDatePicker = new sugoi.form.elements.DatePicker( 'startDate', 'Date de début', catalog.startDate.getTime() > Date.now().getTime() ? catalog.startDate : Date.now() );
-		startDatePicker.format = 'LL';
-		form.addElement( startDatePicker );
-		var endDatePicker = new sugoi.form.elements.DatePicker( 'endDate', 'Date de fin', catalog.endDate );
-		endDatePicker.format = 'LL';
-		form.addElement( endDatePicker );
-
-		if ( form.isValid() ) {
-
-			try {
-				
-				form.toSpod( subscription );			
-				service.SubscriptionService.createSubscription( subscription.user, catalog, subscription.startDate, subscription.endDate );
-				var ordersData = new Array< { id : Int, productId : Int, qt : Float, paid : Bool, invertSharedOrder : Bool, userId2 : Int } >();
-				for ( product in catalogProducts ) {
-
-					var quantity = form.getValueOf( 'product' + product.id );
-					if ( quantity != 0 ) {
-
-						ordersData.push( { id : null, productId : product.id, qt : quantity, paid : false, invertSharedOrder : false, userId2 : null } );
-					}
-					
-				}
-				service.OrderService.createOrUpdateOrders( db.User.manager.get( form.getValueOf( 'userId' ) ), null, catalog, ordersData );
-
-			}
-			catch( error : Error ) {
-				
-				throw Error( '/contractAdmin/subscriptions/insert/' + catalog.id, error.message );
-			}
-
-			throw Ok( '/contractAdmin/subscriptions/' + catalog.id, 'La souscription pour ' + subscription.user.getName() + ' a bien été ajoutée.' );
-		}
-	
-		view.form = form;
-		view.title = 'Nouvelle souscription pour ' + catalog.name;
-		view.c = catalog;
-		view.catalog = catalog;
-		view.nav.push( 'subscriptions' );
 	}
 
 	@tpl("contractadmin/editsubscription.mtt")
@@ -189,7 +95,7 @@ class Subscriptions extends controller.Controller
 					throw Error( '/contractAdmin/subscriptions/insert/' + catalog.id, "Vous devez sélectionner une date de début et de fin pour la souscription." );
 				}
 				
-				var ordersData = new Array< { id : Int, productId : Int, qt : Float, paid : Bool, invertSharedOrder : Bool, userId2 : Int } >();
+				var ordersData = new Array< { productId : Int, qt : Float, invertSharedOrder : Bool, userId2 : Int } >();
 				for ( product in catalogProducts ) {
 
 					var quantity : Float = Std.parseFloat( app.params.get( 'quantity' + product.id ) );
@@ -218,7 +124,7 @@ class Subscriptions extends controller.Controller
 
 					if ( quantity != 0 ) {
 
-						ordersData.push( { id : null, productId : product.id, qt : quantity, paid : false, invertSharedOrder : invert, userId2 : userId2 } );
+						ordersData.push( { productId : product.id, qt : quantity, invertSharedOrder : invert, userId2 : userId2 } );
 					}
 					
 				}
@@ -233,8 +139,7 @@ class Subscriptions extends controller.Controller
 				subscription.catalog = catalog;
 				subscription.startDate = startDate;
 				subscription.endDate = endDate;
-				service.SubscriptionService.createSubscription( user, catalog, startDate, endDate );
-				service.OrderService.createOrUpdateOrders( user, null, catalog, ordersData );
+				service.SubscriptionService.createSubscription( user, catalog, startDate, endDate, ordersData );
 
 				throw Ok( '/contractAdmin/subscriptions/' + catalog.id, 'La souscription pour ' + user.getName() + ' a bien été ajoutée.' );
 
@@ -267,16 +172,55 @@ class Subscriptions extends controller.Controller
 
 		if ( checkToken() ) {
 
-			var startDate = Date.fromString( app.params.get( "startdate" ) );
-			var endDate = Date.fromString( app.params.get( "enddate" ) );
-			if ( startDate == null || endDate == null ) {
-
-				throw Error( '/contractAdmin/subscriptions/edit/' + subscription.id, "Vous devez sélectionner une date de début et de fin pour la souscription." );
-			}
-
 			try {
 
-				service.SubscriptionService.updateSubscription( subscription, startDate, endDate );
+				var startDate = Date.fromString( app.params.get( "startdate" ) );
+				var endDate = Date.fromString( app.params.get( "enddate" ) );
+				if ( startDate == null || endDate == null ) {
+
+					throw Error( '/contractAdmin/subscriptions/edit/' + subscription.id, "Vous devez sélectionner une date de début et de fin pour la souscription." );
+				}
+
+				var ordersData = new Array< { productId : Int, qt : Float, invertSharedOrder : Bool, userId2 : Int } >();
+				for ( product in catalogProducts ) {
+
+					var quantity : Float = Std.parseFloat( app.params.get( 'quantity' + product.id ) );
+					var user2 : db.User = null;
+					var userId2 : Int = Std.parseInt( app.params.get( 'user2' + product.id ) );
+					var invert = false;
+					if ( userId2 != null && userId2 != 0 ) {
+
+						user2 = db.User.manager.get( userId2 );
+						if ( user2 == null ) {
+
+							throw Error( '/contractAdmin/subscriptions/edit/' + subscription.id, t._( "Unable to find user #::num::", { num : userId2 } ) );
+						}
+						if ( !user2.isMemberOf( subscription.catalog.group ) ) {
+
+							throw Error( '/contractAdmin/subscriptions/edit/' + subscription.id, subscription.user + " ne fait pas partie de ce groupe." );
+						}
+						if ( subscription.user.id == user2.id ) {
+							
+							throw Error( '/contractAdmin/subscriptions/edit/' + subscription.id, "Vous ne pouvez pas alterner avec la personne qui a la souscription." );
+						}
+
+						invert = app.params.get( 'invert' + product.id ) == "true";
+
+					}
+
+					if ( quantity != 0 ) {
+
+						ordersData.push( { productId : product.id, qt : quantity, invertSharedOrder : invert, userId2 : userId2 } );
+					}
+					
+				}
+
+				if ( ordersData.length == 0 ) {
+
+					throw Error( '/contractAdmin/subscriptions/edit/' + subscription.id, "Vous devez entrer au moins une quantité pour un produit." );
+				}
+
+				service.SubscriptionService.updateSubscription( subscription, startDate, endDate, ordersData );
 
 			}
 			catch( error : Error ) {
@@ -300,6 +244,7 @@ class Subscriptions extends controller.Controller
 		view.startdate = subscription.startDate;
 		view.enddate = subscription.endDate;
 		view.nav.push( 'subscriptions' );
+
 	}
 
 
