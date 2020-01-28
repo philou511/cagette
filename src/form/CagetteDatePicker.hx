f281ecf000cf6c5e8419ccb097f51990e6fa8e58
JohnDiorroer <fabien@ateliercalico.com>
2020-01-28 15:51:50 +0100

    fix(DatePicker): regex accent

--- src/form/CagetteDatePicker.hx
+++ src/form/CagetteDatePicker.hx
@@ -28,8 +28,8 @@ class CagetteDatePicker extends NativeDatePicker {
     if(str=="") return null;
 
     var date = Date.now();
-    var rDate = ~/([a-z]*) ([0-9]*) ([a-z]*) ([0-9]*)/;
-    var rDatetime = ~/([a-z]*) ([0-9]*) ([a-z]*) ([0-9]*) à ([0-9]*)h([0-9]*)/;
+    var rDate = ~/([A-zÀ-ú]*) ([0-9]*) ([A-zÀ-ú]*) ([0-9]*)/;
+    var rDatetime = ~/([A-zÀ-ú]*) ([0-9]*) ([A-zÀ-ú]*) ([0-9]*) à ([0-9]*)h([0-9]*)/;
     var rTime = ~/([0-9]*)h([0-9])*/;
 
     switch (type) {
