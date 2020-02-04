106405ce5838353c6177ab96dc88dadf736f02d5
JohnDiorroer <fabien@ateliercalico.com>
2020-02-04 14:50:09 +0100

    feat(ShopCart): add

--- js/ShopCart.hx
+++ js/ShopCart.hx
@@ -1,4 +1,3 @@
-import js.html.InputElement;
 import js.Browser;
 import Common;
 import js.jquery.JQuery;
@@ -13,7 +12,7 @@ class ShopCart
 	public var pinnedCategories : Array<{name:String,pinned:Bool,categs:Array<CategoryInfo>}>; //categ db
 	public var order : TmpBasketData;
 	
-	var loader : js.html.Element; //ajax loader gif
+	var loader : JQuery; //ajax loader gif
 	
 	//for scroll mgmt
 	var cartTop : Int;
@@ -35,19 +34,16 @@ class ShopCart
 		pinnedCategories = [];
 	}
 	
-
-	
 	public function add(pid:Int) {
-		loader.style.display = "block";
+		loader.show();
 		
-		var inputEl: InputElement = cast Browser.document.getElementById('productQt' + pid);
-		var q = inputEl.value;
+		var q = untyped Browser.document.getElementById('productQt' + pid).value;
 		var qt = 0.0;
 		var p = this.products.get(pid);
 		if (p.hasFloatQt) {
 			q = StringTools.replace(q, ",", ".");
 			qt = Std.parseFloat(q);
-		}else {
+		} else {
 			qt = Std.parseInt(q);			
 		}
 		
@@ -57,14 +53,10 @@ class ShopCart
 		
 		//add server side
 		var r = new haxe.Http('/shop/add/$multiDistribId/$pid/$qt');
-		
 		r.onData = function(data:String) {
-			loader.style.display = "none";
-
+			loader.hide();
 			var d = haxe.Json.parse(data);
 			if (!d.success) js.Browser.alert("Erreur : "+d);
-			
-			//add locally
 			subAdd(pid, qt);
 			render();
 		}
@@ -73,13 +65,16 @@ class ShopCart
 	
 	
 	function subAdd(pid, qt:Float ) {
+	
 		for ( p in order.products) {
 			if (p.productId == pid) {
+				
 				p.quantity += qt;
 				render();
 				return;
 			}
 		}
+			
 		order.products.push( { productId:pid, quantity:qt } );
 	}
 	
@@ -87,42 +82,24 @@ class ShopCart
 	 * Render the shopping cart and total
 	 */
 	function render() {
-		var c = Browser.document.getElementById("cart");
-		while (c.firstChild != null) c.removeChild(c.firstChild);
-
-		Lambda.map(order.products, function( x ) {
+		var c = App.jq("#cart");
+		c.empty();
+		
+		//render items in shopping cart
+		c.append( Lambda.map(order.products, function( x ) {
 			var p = this.products.get(x.productId);
-			if (p == null) return false;
-
-			var row = Browser.document.createElement("div");
-			row.classList.add("row");
-
-			var col1 = Browser.document.createElement("div");
-			col1.classList.add("order");
-			col1.classList.add("col-md-9");
-			col1.innerHTML = "<b> " + x.quantity + " </b> x " + p.name;
-			row.appendChild(col1);
-
-			var col2 = Browser.document.createElement("div");
-			col2.classList.add("col-md-3");
-			row.appendChild(col2);
-
-			var btn = Browser.document.createElement("a");
-			btn.classList.add("btn");
-			btn.classList.add("btn-default");
-			btn.classList.add("btn-xs");
-			btn.setAttribute("data-toggle", "tooltip");
-			btn.setAttribute("data-placement", "top");
-			btn.setAttribute("title", "Retirer de la commande");
-			btn.innerHTML = "<i class='icon icon-delete'></i>";
-			btn.onclick = function() {
-				remove(p.id);
+			if (p == null) {
+				//the product may have been disabled by an admin
+				return "";
 			}
-			col2.appendChild(btn);
-
-			c.appendChild(row);
-			return true;
-		});
+			
+			var btn = "<a onClick='cart.remove(" + p.id + ")' class='btn btn-default btn-xs' data-toggle='tooltip' data-placement='top' title='Retirer de la commande'><i class='icon icon-delete'></i></a>&nbsp;";
+			return "<div class='row'> 
+				<div class = 'order col-md-9' > <b> " + x.quantity + " </b> x " + p.name+" </div>
+				<div class = 'col-md-3'> "+btn+"</div>			
+			</div>";
+		}).join("\n") );
+		
 		
 		//compute total price
 		var total = 0.0;
@@ -134,10 +111,8 @@ class ShopCart
 		var ffilter = new sugoi.form.filters.FloatFilter();
 		
 		var total = ffilter.filterString(Std.string(App.roundTo(total,2)));
-		var totalEl = Browser.document.createElement("div");
-		totalEl.classList.add("total");
-		totalEl.appendChild(Browser.document.createTextNode("TOTAL : " + total));
-		c.appendChild(totalEl);		
+		c.append("<div class='total'>TOTAL : " + total + "</div>");
+		
 		
 		if (order.products.length > 0){
 			App.instance.setWarningOnUnload(true,"Vous avez une commande en cours. Si vous quittez cette page sans confirmer, votre commande sera perdue.");
@@ -147,8 +122,9 @@ class ShopCart
 	}
 
 
-	function findCategoryName(cid: Int): String {
-		for (cg in this.categories){
+	function findCategoryName(cid:Int):String{
+
+		for ( cg in this.categories ){
 			for (c in cg.categs){
 				if (cid == c.id) {
 					return c.name;
@@ -168,53 +144,68 @@ class ShopCart
 	/**
 	 * Dynamically sort products by categories
 	 */
-	public function sortProductsBy() {
-		trace("sortProductsBy");
+	public function sortProductsBy(){
+
 		//store products by groups
 		var groups = new Map<Int,{name:String,products:Array<ProductInfo>}>();
 		var pinned = new Map<Int,{name:String,products:Array<ProductInfo>}>();
 
 		var firstCategGroup = this.categories[0].categs;
+
+		//trace(firstCategGroup);
+		//trace(pinnedCategories);
+
 		var pList = this.productsArray.copy();
 
-		//sort by categs
-		for (p in pList.copy()) {
-			// untyped p.element.remove();
-			// var element: js.html.Element = untyped p.element; 
-			// while (element.firstChild != null) element.removeChild(element.firstChild);
+		//for ( p in pList) trace(p.name+" : " + p.categories);
+		//trace("----------------");
 
-			for (categ in p.categories){
-				if (Lambda.find(firstCategGroup, function(c) return c.id == categ) != null) {
+		//sort by categs
+		for ( p in pList.copy() ){
+			//trace(p.name+" : " + p.categories);
+			untyped p.element.remove();
+
+			for ( categ in p.categories){
+
+				if (Lambda.find(firstCategGroup, function(c) return c.id == categ) != null){
+
 					//is in this category group
 					var g = groups.get(categ);
-					if (g == null){
+					if ( g == null){
 						var name = findCategoryName(categ);
 						g = {name:name,products:[]};
 					}
 					g.products.push(p);
+					//trace("remove " + p.name);
 					pList.remove(p);
 					groups.set(categ, g);
+
 				}
-				else {
+				else{
 					// is in pinned group ?
 					var isInPinnedCateg = false;
-					for (cg in pinnedCategories){
-						if (Lambda.find(cg.categs, function(c) return c.id == categ) != null) {
-							isInPinnedCateg = true;
-							break;
-						}
+					for ( cg in pinnedCategories){
+							if (Lambda.find(cg.categs, function(c) return c.id == categ) != null){
+								isInPinnedCateg = true;
+								break;
+							}
 					}
 
-					if (isInPinnedCateg) {
+					if (isInPinnedCateg){
+
 						var c = pinned.get(categ);
-						if (c == null){
+						if ( c == null){
+
 							var name = findCategoryName(categ);
 							c = {name:name,products:[]};
 						}
 						c.products.push(p);
+						//trace( "add " + p.name+" in PINNED");
 						pList.remove(p);
 						pinned.set(categ, c);
-					} else {
+
+
+					}else{
 						//not in the selected categ nor in pinned groups
 						continue;
 					}
@@ -226,32 +217,27 @@ class ShopCart
 		if (pList.length > 0){
 			groups.set(0,{name:"Autres",products:pList});
 		}
-
-		// var container = App.jq(".shop .body");
-		var container = Browser.document.querySelector(".shop .body");
-
+		//trace("----------------");
+		//render
+		var container = App.jq(".shop .body");
 		//render firts "pinned" groups , then "groups"
-		for (source in [pinned, groups]) {
-			for (o in source){
-				if (o.products.length == 0) continue;
-				var col = Browser.document.createElement("div");
-				for (c in ["col-md-12", "col-xs-12", "col-sm-12", "col-lg-12"]) col.classList.add(c);
-				col.innerHTML = "<div class='catHeader'>" + o.name + "</div>";
-				container.appendChild(col);
-				// container.append("<div class='col-md-12 col-xs-12 col-sm-12 col-lg-12'><div class='catHeader'>" + o.name + "</div></div>");
+		for ( source in [pinned, groups]){
 
-				for (p in o.products) {
-					trace(untyped p.element.outerHTML);
-				// 	// trace("p", untyped p.element.parentElement.length == 0);
-				// 	//if the element has already been inserted, we need to clone it
-				// 	if (untyped p.element.parentElement.length == 0){
-				// 		container.appendChild(untyped p.element);
-				// 		container.appendChild(Browser.document.createTextNode("HELLO!!"));
-				// 	} else{
-				// 		var clone = untyped p.element.cloneNode(true);
-				// 		container.appendChild(clone);
-				// 		// trace(clone);
-				// 	}
+			for (o in source){
+
+				if (o.products.length == 0) continue;
+				container.append("<div class='col-md-12 col-xs-12 col-sm-12 col-lg-12'><div class='catHeader'>" + o.name + "</div></div>");
+				for ( p in o.products){
+					//trace("GROUP "+o.name+" : "+p.name);
+					//if the element has already been inserted, we need to clone it
+					if (untyped p.element.parent().length == 0){
+						container.append( untyped p.element );
+					}else{
+						var clone = untyped p.element.clone();
+						container.append( clone );
+					}
+
+
 				}
 			}
 		}
@@ -300,6 +286,10 @@ class ShopCart
 				App.jq(".shop .product" + p.id).fadeOut(300);
 			}
 		}
+		
+		
+		
+		
 	}
 	
 	/**
@@ -307,11 +297,16 @@ class ShopCart
 	 * @param	pid
 	 */
 	public function remove(pid:Int ) {
-		loader.style.display = "block";
+		
+		loader.show();
+		
 		//add server side
 		var r = new haxe.Http('/shop/remove/$multiDistribId/$pid');
+		
 		r.onData = function(data:String) {
-			loader.style.display = "none";
+			
+			loader.hide();
+			
 			var d = haxe.Json.parse(data);
 			if (!d.success) js.Browser.alert("Erreur : "+d);
 			
@@ -324,20 +319,30 @@ class ShopCart
 				}
 			}
 			render();
+			
+			
 		}
 		r.request();
+		
+		
+		
+		
 	}
 	
 	/**
 	 * loads products DB and existing cart in ajax
 	 */
 	public function init(multiDistribId:Int) {
+
+		// this.place = place;
+		// this.date = date;
 		this.multiDistribId = multiDistribId;
-		loader = Browser.document.getElementById("loader");
+		
+		loader = App.jq("#cartContainer #loader");
 		
 		var req = new haxe.Http("/shop/init/"+multiDistribId);
 		req.onData = function(data) {
-			loader.style.display = "none";
+			loader.hide();
 			
 			var data : { 
 				products:Array<ProductInfo>,
@@ -356,11 +361,14 @@ class ShopCart
 			//product DB
 			for (p in data.products) {
 				//catch dom element for further usage
-				untyped p.element = Browser.document.querySelector(".product" + p.id);
+				untyped p.element = App.jq(".product"+p.id);
 
 				var id : Int = p.id;
+				//var id : Int = p.id;
+ 				//id = id + 1;
 				this.products.set(id, p);
 				this.productsArray.push(p);
+				//trace(p.name+" : " + p.categories);
 			}
 			
 			//existing order
@@ -369,6 +377,7 @@ class ShopCart
 			}
 			
 			render();
+			
 			sortProductsBy();
 
 		}
@@ -394,23 +403,26 @@ class ShopCart
 	 * keep the cart on top when scrolling
 	 * @param	e
 	 */
-	// public function onScroll(e:Dynamic) {
+	public function onScroll(e:Dynamic) {
 		
-	// 	//cart container top position		
+		//cart container top position		
 		
-	// 	if (jWindow.scrollTop() > cartTop) {
-	// 		//trace("absolute !");
-	// 		cartContainer.addClass("scrolled");
-	// 		cartContainer.css('left', Std.string(cartLeft) + "px");			
-	// 		cartContainer.css('top', Std.string(/*cartTop*/10) + "px");
-	// 		cartContainer.css('width', Std.string(cartWidth) + "px");
+		if (jWindow.scrollTop() > cartTop) {
+			//trace("absolute !");
+			cartContainer.addClass("scrolled");
+			cartContainer.css('left', Std.string(cartLeft) + "px");			
+			cartContainer.css('top', Std.string(/*cartTop*/10) + "px");
+			cartContainer.css('width', Std.string(cartWidth) + "px");
 			
-	// 	}else {
-	// 		cartContainer.removeClass("scrolled");
-	// 		cartContainer.css('left',"");
-	// 		cartContainer.css('top', "");
-	// 		cartContainer.css('width', "");
-	// 	}
-	// }
+		}else {
+			cartContainer.removeClass("scrolled");
+			cartContainer.css('left',"");
+			cartContainer.css('top', "");
+			cartContainer.css('width', "");
+		}
+		
+		
+		
+	}
 	
 }
