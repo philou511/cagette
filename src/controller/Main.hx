bba8241d7e0a89098ca92a00733a464db79cc113
François Barbut <francois@alilo.fr>
2020-12-04 11:36:07 +0100

    update cgu and cgv links

--- src/controller/Main.hx
+++ src/controller/Main.hx
@@ -347,16 +347,15 @@ Called from controller/Main.hx line 117
 	function doDebug(d:Dispatch) {
 		d.dispatch(new controller.Debug());
 	}
-
 	
 	//CGU
 	public function doCgu() {
-		throw Redirect("https://www.cagette.net/wp-content/uploads/2020/10/cgu.pdf");
+		throw Redirect("https://www.cagette.net/wp-content/uploads/2020/11/cgu-.pdf");
 	}
 
 	//CGV
 	public function doCgv() {
-		throw Redirect("https://www.cagette.net/wp-content/uploads/2020/10/cgv.pdf");
+		throw Redirect("https://www.cagette.net/wp-content/uploads/2020/11/cgv.pdf");
 	}
 
 	//CGU MGP
