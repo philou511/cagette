226d2490e32f1953d6794bc8f27118f6d48df072
Corentin <corentincloarec@hotmail.fr>
2021-05-18 16:08:22 +0200

    Use translated LoginRegistration module

--- js/App.hx
+++ js/App.hx
@@ -276,43 +276,27 @@ class App {
 	/**
 	 * Displays a login box
 	 */
-	public function loginBox(redirectUrl:String,?message:String,?phoneRequired=false,?addressRequired=false) {	
-        // //initSentry();
-
-		var modalElement = Browser.document.getElementById("myModal");
-		modalElement.querySelector(".modal-title").innerHTML = "S'identifier";
-		modalElement.querySelector(".modal-dialog").classList.remove("modal-lg");
-		var modal = new bootstrap.Modal(modalElement);
-		modal.show();
-
-		ReactDOM.render(
-			jsx('<$LoginBox redirectUrl=$redirectUrl message=$message phoneRequired=$phoneRequired addressRequired=$addressRequired/>'),
-			js.Browser.document.querySelector('#myModal .modal-body')
-		);
-		return false;
+	public function loginBox(redirectUrl:String,?message:String,?phoneRequired=false,?addressRequired=false,?openRegistration=false) {	
+        var node = js.Browser.document.createDivElement();
+		node.id = "login-registration-container";
+		js.Browser.document.body.appendChild(node);
+		ReactDOM.unmountComponentAtNode(node); //the previous modal DOM element is still there, so we need to destroy it
+        
+        var neo:Dynamic = Reflect.field(js.Browser.window, 'neo');
+        neo.createNeoModule(node.id, "loginRegistration", {
+            redirectUrl: redirectUrl,
+			message: message,
+			phoneRequired: phoneRequired,
+			addressRequired: addressRequired,
+            openRegistration: openRegistration
+        });
 	}
 
 	/**
 	 *  Displays a sign up box
 	 */
-	public function registerBox(redirectUrl:String,?message:String,?phoneRequired=false,?addressRequired=false,?tmpBasketId:Int) {
-        //initSentry();
-
-		var modalElement = Browser.document.getElementById("myModal");
-		modalElement.querySelector(".modal-title").innerHTML = "S'inscrire";
-		modalElement.querySelector(".modal-dialog").classList.remove("modal-lg");
-		var modal = new bootstrap.Modal(modalElement);
-        modal.show();
-        modalElement.addEventListener('hide.bs.modal', function() {
-            if (tmpBasketId!=null){
-                js.Browser.window.location.href = "/transaction/tmpBasket/"+tmpBasketId;
-            }
-        });
-		ReactDOM.render(
-			jsx('<$RegisterBox redirectUrl=$redirectUrl message=$message phoneRequired=$phoneRequired addressRequired=$addressRequired/>'),
-			js.Browser.document.querySelector('#myModal .modal-body')
-		);
-		return false;
+	public function registerBox(redirectUrl:String,?message:String,?phoneRequired=false,?addressRequired=false) {
+        loginBox(redirectUrl, message, phoneRequired, addressRequired, true);
 	}
 
 	public function membershipBox(userId:Int,userName:String,groupId:Int,?callbackUrl:String,?distributionId:Int){
