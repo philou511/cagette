74daa96eaccccb677dd3cbf791429ca6bf256c17
François Barbut <francois@alilo.fr>
2021-10-14 16:51:29 +0200

    wip

--- plugins/pro/controller/CatalogLinker.hx
+++ plugins/pro/controller/CatalogLinker.hx
@@ -16,40 +16,65 @@ class CatalogLinker extends controller.Controller
 		view.vendor = vendor = pro.db.CagettePro.getCurrentVendor();
 	}
 		
+	/**
+		choose which invited catalog to link
+	**/
 	@logged @tpl("plugin/pro/catalogLinker/default.mtt")
 	public function doDefault(){
 				
-
 		view.unlinkedCatalogs = VendorService.getUnlinkedCatalogs(company);
-
 		view.vendorId = vendor.id;
 	}
 
-    @logged @tpl("plugin/pro/catalogLinker/link.mtt")
-	public function doLink(catalog:db.Catalog){
+	@logged @tpl("plugin/pro/catalogLinker/selectPCatalog.mtt")
+	public function doSelectPcatalog(c:db.Catalog){
 				
-		view.pcatalog = company.getCatalogs().array()[0];
-        view.catalog = catalog;
+		view.pcatalogs = company.getCatalogs();
+		view.catalog = c;
+		
+	}
 
+	/**
+		linking tool
+	**/
+    @logged @tpl("plugin/pro/catalogLinker/link.mtt")
+	public function doLink(catalog:db.Catalog, pcatalog:pro.db.PCatalog){
+				
+		view.pcatalog = pcatalog;
+        view.catalog = catalog;
+		checkToken();
 	}
 
 	@logged @tpl("plugin/pro/catalogLinker/importFirstCatalog.mtt")
 	public function doImportFirstCatalog(?catalog:db.Catalog){
 
 		if(catalog!=null){
+			if(company.getProducts().length>0) throw Error("/p/pro","Action interdite, vous avez déjà des produits dans votre compte producteur");
 
 			pro.service.PCatalogService.linkRemoteCatalog(catalog,company);
 			throw Ok('/p/pro/product',"Bravo, vous avez récupéré votre premier catalogue ! Vérifiez que les fiches produits sont correctes.");
 
-
 		}else{
 			view.unlinkedCatalogs = VendorService.getUnlinkedCatalogs(company);
 		}
-		
+	}
+
+	public function doSubmitLinkage(remoteCatalog:db.Catalog,pcatalog:pro.db.PCatalog){
+
+		if(checkToken()){
+			var linkage = [];
+			for( k in app.params.keys()){
+				if(k.substr(0,1)=="p"){
+					linkage.push({
+						productId:k.substr(1),
+						offerId:app.params.get(k)
+					});
+				}
+			}
+
+			trace(linkage);
+		}
 
 	}
 
-
-	
-	
 }
