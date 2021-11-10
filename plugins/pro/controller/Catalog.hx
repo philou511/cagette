6729a08ee36c132f6979b94cd5e595605dd160bb
Corentin <corentincloarec@hotmail.fr>
2021-11-10 14:30:40 +0100

    Remove deliveryAvailabilities

--- plugins/pro/controller/Catalog.hx
+++ plugins/pro/controller/Catalog.hx
@@ -144,47 +144,47 @@ class Catalog extends controller.Controller
 		}
 	}
 	
-	@tpl('plugin/pro/catalog/conditions.mtt')
-	function doConditions(c:pro.db.PCatalog){
-		checkRights(c);
-		view.nav.push("conditions");
-		view.catalog = c;
+	// @tpl('plugin/pro/catalog/conditions.mtt')
+	// function doConditions(c:pro.db.PCatalog){
+	// 	checkRights(c);
+	// 	view.nav.push("conditions");
+	// 	view.catalog = c;
 		
-		view.data = c.deliveryAvailabilities;
+	// 	view.data = c.deliveryAvailabilities;
 		
-		if (app.params.exists("submit")){
-			//delivery availabilities
-			var data = new Array<{startHour:Int,startMinutes:Int,endHour:Int,endMinutes:Int}>();
-			for ( d in 0...7){
+	// 	if (app.params.exists("submit")){
+	// 		//delivery availabilities
+	// 		var data = new Array<{startHour:Int,startMinutes:Int,endHour:Int,endMinutes:Int}>();
+	// 		for ( d in 0...7){
 				
-				if (app.params.exists("day" + d)){
-					data[d] = {
-						startHour:app.params.get('startHour' + d).parseInt(),
-						startMinutes:app.params.get('startMinutes' + d).parseInt(),
-						endHour:app.params.get('endHour' + d).parseInt(),
-						endMinutes:app.params.get('endMinutes' + d).parseInt(),
+	// 			if (app.params.exists("day" + d)){
+	// 				data[d] = {
+	// 					startHour:app.params.get('startHour' + d).parseInt(),
+	// 					startMinutes:app.params.get('startMinutes' + d).parseInt(),
+	// 					endHour:app.params.get('endHour' + d).parseInt(),
+	// 					endMinutes:app.params.get('endMinutes' + d).parseInt(),
 					
-					};
+	// 				};
 					
-				}
-			}
+	// 			}
+	// 		}
 			
-			c.lock();
-			if ( c.deliveryAvailabilities == null) c.deliveryAvailabilities = [];
-			c.deliveryAvailabilities = data;
+	// 		c.lock();
+	// 		if ( c.deliveryAvailabilities == null) c.deliveryAvailabilities = [];
+	// 		c.deliveryAvailabilities = data;
 			
-			//distance
-			if (app.params.exists("maxDistance")){
-				var d = Std.parseInt(app.params.get("maxDistance"));
-				c.maxDistance = d;
-			}else{
-				c.maxDistance = null;
-			}
+	// 		//distance
+	// 		if (app.params.exists("maxDistance")){
+	// 			var d = Std.parseInt(app.params.get("maxDistance"));
+	// 			c.maxDistance = d;
+	// 		}else{
+	// 			c.maxDistance = null;
+	// 		}
 			
-			c.update();
-			throw Ok("/p/pro/catalog/publish/" + c.id, "Conditions mises à jour");
-		}
-	}
+	// 		c.update();
+	// 		throw Ok("/p/pro/catalog/publish/" + c.id, "Conditions mises à jour");
+	// 	}
+	// }
 
 	/**
 	Create a catalog
