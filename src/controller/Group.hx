d999213a89173a5863ab42d9f16558019d2015a3
Corentin <corentincloarec@hotmail.fr>
2022-07-06 15:21:22 +0200

    Allow creating group when onlyVendorsCanCreateGroup settings is off or not set

--- src/controller/Group.hx
+++ src/controller/Group.hx
@@ -1,7 +1,7 @@
 package controller;
-import payment.Cash;
 import Common;
 import db.Group;
+import payment.Cash;
 import service.BridgeService;
 import service.DistributionService;
 import service.OrderService;
@@ -110,7 +110,10 @@ class Group extends controller.Controller
 	@tpl("form.mtt")
 	function doCreate() {
 		var cagettePros = service.VendorService.getCagetteProFromUser(App.current.user);
-		if (!(App.current.settings.onlyVendorsCanCreateGroup==true && cagettePros!=null && cagettePros.length>0)) {
+		if (!(App.current.settings.onlyVendorsCanCreateGroup==null
+			 || App.current.settings.onlyVendorsCanCreateGroup==false 
+			 || (App.current.settings.onlyVendorsCanCreateGroup==true && cagettePros!=null && cagettePros.length>0))
+			 ) {
 			throw Redirect("/");
 		}
 
