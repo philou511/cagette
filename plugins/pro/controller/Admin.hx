6f53b62aa1afdfffbf5dac8ecb34cd161b2ae545
François Barbut <francois@alilo.fr>
2022-07-19 15:56:52 +0200

    prepare vrac migration

--- plugins/pro/controller/Admin.hx
+++ plugins/pro/controller/Admin.hx
@@ -1646,4 +1646,65 @@ class Admin extends controller.Controller {
 			}
 		}
 	}
+
+	function doVrac(){
+
+		/*var themeId = App.current.getTheme().id;
+
+		var vendorIds = [
+			12640, //Bordeaux
+			13715, //Lyon
+			14000, //Strasbourg
+			14001, //Toulouse
+			14002, //Paris
+			21640, //rennes
+			22409, //nantes
+			23371, //marseille
+			24068, //st etienne
+			24225, //haut de france
+			24226, //montpellier
+			24482, //bruxelles
+			24697, //lyon 2
+			//drome reliée à Lyon
+			//finistere
+		];
+
+		var vendors = db.Vendor.manager.search($id in vendorIds);
+		var print = controller.Cron.print;
+		var cpros = vendors.map( v -> CagettePro.getFromVendor(v));
+		var groups = [];
+		print("==== CPRO");
+		if(themeId=="cagette"){
+			for(cpro in cpros){
+				print(cpro.vendor.name);
+
+				for( g in cpro.getGroups()){
+					print("- "+g.name);
+					groups.push(g);
+
+					
+					//RUN THIS ON CAGETTE.NET
+					// g.lock();
+					// g.disabled = XXX;
+					// g.extUrl = "https://epicerie.vrac-asso.org/group/"+g.id;
+					// g.update();
+					
+				}
+			}
+		}
+
+		//RUN THIS ON VRAC
+		if(themeId=="vrac"){
+			var gids:Array<Int> = groups.map(g -> g.id);
+			var groupsToDelete = db.Group.manager.unsafeObjects('select * from `Group` where id not in (${gids.join(",")}) LIMIT 100',true);
+			print("====  100 Groupes a effacer");
+			for(g in groupsToDelete){
+				print(g.name);
+				//g.delete();
+			}
+		}*/
+		
+
+
+	}
 }
