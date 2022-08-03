9ae60a52be6817bff8f716c431ef166ab7d1486a
François Barbut <francois@alilo.fr>
2022-08-03 10:08:49 +0200

    mgp refund bridge

--- src/controller/api/Distributions.hx
+++ src/controller/api/Distributions.hx
@@ -1,5 +1,8 @@
 package controller.api;
 
+import pro.payment.MangopayECPayment;
+import mangopay.Mangopay;
+import mangopay.MangopayPlugin;
 import haxe.DynamicAccess;
 import tink.core.Error;
 import db.UserGroup;
@@ -11,7 +14,6 @@ import service.DistributionService;
 
 class Distributions extends Controller {
 	
-
 	public function new() {
 		super();
 	}
@@ -65,10 +67,92 @@ class Distributions extends Controller {
 				}
 			}
 			o.volunteerForRole = volunteerForRole;
-			
 			out.multiDistribs.push(o);
 		}
 
 		json(out);
+	}
+
+	/**
+		bridge from TS for mangopay refunds
+	**/
+	function doMangopayRefund(amountToRefund:Float,basket:db.Basket,key:String){
+
+		if(key != haxe.crypto.Md5.encode(App.config.get("key")+"bridge")){
+			throw new tink.core.Error("invalid bridge key");
+		}
+
+		if (MangopayPlugin.hasOnlyMangopayPayments(basket)) {
+					
+			var mangopayUser = mangopay.db.MangopayUser.get(basket.getUser());
+			if( mangopayUser == null ) throw "This user should have a Mangopay user ID";
+
+			//We sort the payments operations by payment amounts so that we can refund first on the biggest amount
+			var sortedPaymentsOperations = basket.getPaymentsOperations().array();
+			sortedPaymentsOperations.sort(function(a, b): Int {
+				if (a.amount < b.amount) return 1;
+				else if (a.amount > b.amount) return -1;
+				return 0;
+			});
+
+			//Let's do the refund on different payments if needed
+			var refundRemainingAmount = Math.abs(amountToRefund);
+			var totalRefunded = 0.0;
+			var refundOps = [];
+			for ( payment in sortedPaymentsOperations ) {
+				var amountToRefund = 0.0;
+				if ( refundRemainingAmount == 0.0 ) break;
+				
+				if ( refundRemainingAmount <= payment.amount ) {
+					//Partial Refund
+					amountToRefund = refundRemainingAmount;
+				} else {
+					//Full Refund						
+					amountToRefund = payment.amount;						
+				}
+				refundRemainingAmount -= amountToRefund;	
+
+				//Let's do the refund for this payment
+				var conf = MangopayPlugin.getGroupConfig(basket.getGroup());
+				var amount = MangopayPlugin.getAmountAndFees(amountToRefund,conf);
+				var refund : mangopay.Types.Refund = {				
+					DebitedFunds: {				
+						Currency: Euro,
+						Amount: Math.round(amount.netAmount * 100)		
+					},
+					Fees: {				
+						Currency: Euro,
+						Amount: 0 - Math.round(amount.fees * 100),//negative : refund fees
+					},
+					AuthorId: mangopayUser.mangopayUserId,
+					InitialTransactionId: payment.getPaymentData().remoteOpId				
+				};				
+				refund = Mangopay.createPayInRefund(refund);				
+				totalRefunded += amountToRefund;
+
+				//create one operation for each refund
+				var op  = new db.Operation();
+				op.type = db.Operation.OperationType.Payment;
+				op.setPaymentData({type:MangopayECPayment.TYPE,remoteOpId: refund.Id.string()});
+				op.name = 'Remboursement (${refund.Id})';
+				op.group = basket.getGroup();
+				op.user = basket.user;
+				op.relation = basket.getOrderOperation();
+				op.amount = 0 - Math.abs(amountToRefund);						
+				op.date = Date.now();
+				op.insert();
+				refundOps.push(op);
+			}
+
+			//prepare operations for JSON serialization
+			var refundOps = refundOps.map(op -> {id:op.id,data:op.getData()});
+
+			json({refunds:refundOps});
+
+		}else{
+			throw new tink.core.Error("Basket #"+basket.id+" is not a mangopay paid basket.");
+		}
+
+
 	}
 }
